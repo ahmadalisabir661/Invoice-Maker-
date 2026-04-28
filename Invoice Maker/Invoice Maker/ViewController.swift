7b1a46e970e3807f649901954429cfafa4ae38b3
ahmadalisabir661 <ahmadalisabir661@gmail.com>
2026-04-28 20:46:54 +0500

    first commit

---
+++ Invoice Maker/Invoice Maker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Invoice Maker
+//
+//  Created by Dev-Mac on 2026-04-28.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
